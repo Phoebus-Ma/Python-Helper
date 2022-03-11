57ae4841785984016d4a407fcee441473fc769d4
Phoebus <tianjindahai@126.com>
2022-03-12 00:23:28 +0800

    [PY] Improve write-file example.

--- FileIO.PY/README.md
+++ FileIO.PY/README.md
@@ -13,12 +13,39 @@ Python 3 for Windows/Linux/MacOS.
 ```console
 $ python read-file.py
 
+0 1 2
+--------------
+0 1 2 3 456 789
 
+Hello, Everybody.
+
+This is python file io test file.
+
+
+--------------
+0 1 2 3 456 789
+Hello, Everybody.
+This is python file io test file.
+--------------
+0 1 2 3 456 789
+
+Hello, Everybody.
+
+This is python file io test file.
 ```
 
 2. Write file Test:
 ```console
 $ python write-file.py
 
-
+----Test 1----
+0 1 2 3 456 789
+Hello, Everybody.
+This is python file io test file.
+To be no.1.
+----Test 2----
+Do better every day.
+----Test 3----
+Create file success.
+Find the file.
 ```
--- FileIO.PY/write-file.py
+++ FileIO.PY/write-file.py
@@ -1,5 +1,5 @@
 ###
-# Read file example.
+# Write file example.
 # 
 # License - MIT.
 ###
@@ -7,10 +7,59 @@
 import os
 
 
-# Main function.
-def main():
+# Verify read.
+def verify_read():
 # {
-    f = open('testfile.txt', 'a')
+    f = open(testfile, 'r')
+
+    data = f.read()
+    print(data)
+
+    f.close()
+# }
+
+
+# Write test 3.
+def write_test3():
+# {
+    # Create.
+    # 'x' - Create, create a file, returns an error if the file exist.
+    # 'w' - Write, create a file if the specified file does not exist.
+    # 'a' - Append, create a file if the specified file does not exist.
+    create_file = 'myfile.txt'
+
+    try:
+        f = open(create_file, 'x')
+        print('Create file success.')
+
+        f.close()
+    except:
+        print('File exist.')
+
+    if True == os.path.exists(create_file):
+        print('Find the file.')
+    else:
+        print('No file found.')
+# }
+
+
+# Write test 2.
+def write_test2():
+# {
+    # Overwrite.
+    f = open(testfile, 'w')
+
+    f.write('Do better every day.')
+
+    f.close()
+# }
+
+
+# Write test 1.
+def write_test1():
+# {
+    # Append.
+    f = open(testfile, 'a')
 
     f.write('\nTo be no.1.')
 
@@ -18,6 +67,23 @@ def main():
 # }
 
 
+# Main function.
+def main():
+# {
+    print('----Test 1----')
+    write_test1()
+    verify_read()
+
+    print('----Test 2----')
+    write_test2()
+    verify_read()
+
+    print('----Test 3----')
+    write_test3()
+# }
+
+
 # Program entry.
 if '__main__' == __name__:
+    testfile = 'testfile.txt'
     main()
