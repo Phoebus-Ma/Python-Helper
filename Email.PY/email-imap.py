d067a573daf7d39048e11836d087379c54a278ef
Phoebus <tianjindahai@126.com>
2022-06-29 09:18:17 +0800

    [PY] Update use IMAP receive email.

--- Email.PY/email-imap.py
+++ Email.PY/email-imap.py
@@ -5,15 +5,40 @@
 ###
 
 import os
+import imaplib
+import pprint
 
 
 # Main function.
 def main():
 # {
-    pass
+    print('Login QQ email:')
+    imap_user = input('Username:')
+    imap_pass = input('Password:')
+
+    imap = imaplib.IMAP4_SSL(host = imap_host, port = 993)
+
+    imap.login(imap_user, imap_pass)
+
+    imap.select('Inbox')
+
+    tmp, data = imap.search(None, 'ALL')
+
+    for num in data[0].split():
+        tmp, data = imap.fetch(num, '(RFC822)')
+        print('Message: {0}\n'.format(num))
+        # pprint.pprint(data[0][1])
+        break
+
+    imap.close()
+    imap.logout()
 # }
 
 
 # Program entry.
 if '__main__' == __name__:
+# {
+    imap_host = 'imap.qq.com'
+
     main()
+# }
